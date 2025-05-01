33623a97591f720f261b5d030c3a01da0031bfb1
ramazandautbek <ramazandautbek04@gmail.com>
2025-05-01 19:10:54 +0500

    chor(u_handler): set cookie domain was changed

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -70,7 +70,7 @@ func (h *AuthHandler) Login(c *gin.Context) {
 		Name:     "authToken",
 		Value:    token,
 		Path:     "/",
-		Domain:   ".up.railway.app",
+		Domain:   ".vercel.app",
 		HttpOnly: true,
 		Secure:   true,
 		SameSite: http.SameSiteNoneMode,
